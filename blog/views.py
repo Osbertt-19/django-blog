5d33277d8651b77eacebd50787e933f8fe0d4d75
Osbertt <minhtetnaing25mhn@gmail.com>
2022-08-13 03:22:44 +0630

    removed unnecessary get_obj_or_404

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, get_object_or_404
+from django.shortcuts import render, redirect
 from .models import Post, Comment, Tag
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
