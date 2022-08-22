7af9b671035a6e4d8f1564889ccc9846cb1bee0f
Osbertt <minhtetnaing25mhn@gmail.com>
2022-08-23 02:26:36 +0630

    Added humanize

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
+    'django.contrib.humanize',
     'rest_framework',
     'blog.apps.BlogConfig',
     'api.apps.ApiConfig',
